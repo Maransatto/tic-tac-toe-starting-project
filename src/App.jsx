0c78b54ec3cb724f57b1678c08e28b0a6308cc0a
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2024-11-02 18:54:54 -0300

    feat: :sparkles: 90 lifting computed values up

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import Player from "./components/Player";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
 
@@ -18,6 +24,21 @@ function App() {
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol 
+    const secondSquareSymbol 
+    const thirdSquareSymbol 
+  }
 
   function selectSquareHandler(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
@@ -47,7 +68,7 @@ function App() {
           />
         </ol>
 
-        <GameBoard onSelectSquare={selectSquareHandler} turns={gameTurns} />
+        <GameBoard onSelectSquare={selectSquareHandler} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
