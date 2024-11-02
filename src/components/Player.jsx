b10a365da60b25edb764fcb772223471ef15f0fb
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2024-11-02 16:30:27 -0300

    feat: :sparkles: 79 user input & two-way-binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,22 +1,29 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function toggleEditingHandler() {
     setIsEditing((editing) => !editing);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function changeHandler(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required value={name} />;
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={changeHandler} />
+    );
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={toggleEditingHandler}>
