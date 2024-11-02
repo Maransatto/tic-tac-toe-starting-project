7d5e38ea362bc044dffaf5dbeed458624d0240dc
Fernando Silva Maransatto <fernando.maransatto@gmail.com>
2024-11-02 19:02:08 -0300

    feat: :sparkles: 91 deriving computed values from other computed values

--- src/App.jsx
+++ src/App.jsx
@@ -34,10 +34,23 @@ function App() {
     gameBoard[row][col] = player;
   }
 
+  let winner = null;
+
   for (const combination of WINNING_COMBINATIONS) {
-    const firstSquareSymbol 
-    const secondSquareSymbol 
-    const thirdSquareSymbol 
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
   }
 
   function selectSquareHandler(rowIndex, colIndex) {
@@ -67,7 +80,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-
+        {winner && <p>You won, {winner}! </p>}
         <GameBoard onSelectSquare={selectSquareHandler} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
